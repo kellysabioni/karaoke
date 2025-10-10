3caf4ef7f05ab09585aac08023db22935e2187a0
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-10-10 00:22:16 -0300

    Cria styles colors e global
    Iniciando estilos das paginas

---
+++ src/styles/colors.js
@@ -0,0 +1,12 @@
+// src/styles/colors.js
+export const colors = {
+  primariaClara: "#4E2A57",
+  primaria: "#36173D",
+  primariaEscura: "#201124",
+  secundaria: "#FFC55F",
+  secundariaEscura: "#FFC107",
+  texto: "#121212",
+  white: "#Fafafa",
+  alerta: "#DC3545",
+  alerta2: "#FF4845",
+};
