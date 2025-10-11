3bb0a6211778e11d549ce740998b4e2232597ce5
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-10-11 00:59:44 -0300

    Cria paginas perfil e resultado busca

---
+++ app/perfil.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function perfil() {
+  return (
+    <View>
+      <Text>PERFIL</Text>
+    </View>
+  );
+}
---
+++ app/resultado.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function resultado() {
+  return (
+    <View>
+      <Text>RESULTADO DA PESQUISA</Text>
+    </View>
+  );
+}
