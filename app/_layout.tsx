82bba65674ae112a30b09ff1fc089df6525d6ec3
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-10-11 00:22:36 -0300

    Corrige codigo excluindo informações  erradas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,8 @@
-import { useFonts } from "expo-font";
-import Loading from "../src/components/Loading";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
-  const [fonteCarregada] = useFonts({
-    // Adicione suas fontes personalizadas aqui
-    // Monoton: require("@/assets/fonts/Monoton-Regular.ttf"),
-  });
-
-  if (!fonteCarregada) {
-    return <Loading />;
-  }
-
   return (
     <SafeAreaProvider>
       <StatusBar style="light" backgroundColor="#36173D" />
