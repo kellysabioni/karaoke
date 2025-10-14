a07fac4198a83c580bba5e1b36e361e19ca0d00c
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-10-13 23:11:27 -0300

    Atualiza arquivos para utilização do supabase

--- src/supabase/client.ts
+++
@@ -1,7 +0,0 @@
-// src/supabase/client.ts
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://kutemjrcgimvduaqihdy.supabase.co";
-const supabaseAnonKey = "Senac@123";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
